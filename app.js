1065d7917d2d7395905b9fca46a698b582dd2a74
shakil islam <freelancershakil913@gmail.com>
2022-01-23 15:52:27 +0600

    Till Get Clicked Seat and Selected Seat

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = Vue.createApp({
                 },
                 booked: {
                   text: "Booked",
-                  color: "yellow"
+                  color: "rgb(114, 109, 177)"
                 },
                 available: {
                     text: "Available",
@@ -19,9 +19,138 @@ const app = Vue.createApp({
                   text: "Selected",
                   color: "green"
                 }
-            }
+            },
+            seats: [
+                {
+                  name: "A1",
+                  type: "available",
+                  price: 500
+                },
+                {
+                  name: "A2",
+                  type: "available",
+                  price: 500
+                },
+                {
+                  name: "A3",
+                  type: "available",
+                  price: 500
+                },
+                {
+                  name: "A4",
+                  type: "available",
+                  price: 500
+                },
+                {
+                  name: "B1",
+                  type: "available",
+                  price: 450
+                },
+                {
+                  name: "B2",
+                  type: "available",
+                  price: 450
+                },
+                {
+                  name: "B3",
+                  type: "available",
+                  price: 450
+                },
+                {
+                  name: "B4",
+                  type: "available",
+                  price: 450
+                },
+                {
+                  name: "C1",
+                  type: "sold",
+                  price: 500
+                },
+                {
+                  name: "C2",
+                  type: "sold",
+                  price: 500
+                },
+                {
+                  name: "C3",
+                  type: "sold",
+                  price: 500
+                },
+                {
+                  name: "C4",
+                  type: "sold",
+                  price: 500
+                },
+                {
+                  name: "D1",
+                  type: "available",
+                  price: 400
+                },
+                {
+                  name: "D2",
+                  type: "available",
+                  price: 400
+                },
+                {
+                  name: "D3",
+                  type: "available",
+                  price: 400
+                },
+                {
+                  name: "D4",
+                  type: "available",
+                  price: 400
+                },
+                {
+                  name: "E1",
+                  type: "available",
+                  price: 300
+                },
+                {
+                  name: "E2",
+                  type: "available",
+                  price: 300
+                },
+                {
+                  name: "E3",
+                  type: "booked",
+                  price: 300
+                },
+                {
+                  name: "E4",
+                  type: "booked",
+                  price: 300
+                },
+                {
+                  name: "F1",
+                  type: "available",
+                  price: 300
+                },
+                {
+                  name: "F2",
+                  type: "available",
+                  price: 300
+                },
+                {
+                  name: "F3",
+                  type: "available",
+                  price: 300
+                },
+                {
+                  name: "F4",
+                  type: "sold",
+                  price: 300
+                }
+              ],
         }
-    }
+    },
+    methods: {
+        handleClick(i){
+            let clickedSeat = this.seats[i];
+            clickedSeat.type = "selected"
+            console.log(clickedSeat);
+        }
+    },
 })
 
 app.mount('#app');
