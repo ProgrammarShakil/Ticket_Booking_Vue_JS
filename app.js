e748be65bcbe67471a57e55c5e428b671a44e052
shakil islam <freelancershakil913@gmail.com>
2022-01-23 14:11:39 +0600

    show seat position controll using object and for loop - with style binding

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ const app = Vue.createApp({
     
     data(){
         return{
-
+            seatControls: {
+                sold: {
+                  text: "Sold",
+                  color: "#ff0000"
+                },
+                booked: {
+                  text: "Booked",
+                  color: "yellow"
+                },
+                available: {
+                    text: "Available",
+                    color: "#fff"
+                  },
+                selected: {
+                  text: "Selected",
+                  color: "green"
+                }
+            }
         }
     }
 })
